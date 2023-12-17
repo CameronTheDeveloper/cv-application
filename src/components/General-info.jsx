18bc843329c6b71a693e4fec56ce79e2a57dcd3b
CameronTheDeveloper <Cameronposey@outlook.com>
2023-12-17 03:46:54 -0800

    Add General_mid component with firstName input

--- src/components/General-info.jsx
+++ src/components/General-info.jsx
@@ -4,12 +4,37 @@ function General_top() {
     return (
         <h1>General Information</h1>
     );
+}
 
+function General_mid() {
+    const [firstName, setFirstName] = useState('');
+
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value);
+    }
+
+    return (
+        <form>
+            <label>First Name:
+                <input
+                    type='text'
+                    id='firstNameInput'
+                    name='firstNameInput'
+                    value={firstName}
+                    onChange={handleFirstNameChange}>
+                </input>
+            </label>
+            <button type='submit'>submit</button>
+        </form>
+    );
 }
 
 function GeneralInfo() {
     return (
-        <General_top></General_top>
+        <>
+            <General_top></General_top>
+            <General_mid></General_mid>
+        </>
     );
 }
 
