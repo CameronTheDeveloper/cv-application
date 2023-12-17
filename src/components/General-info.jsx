fce5b3092f509c5d98e3d1ac532166b3f06561a3
CameronTheDeveloper <Cameronposey@outlook.com>
2023-12-16 16:08:32 -0800

    Add GeneralInfo with General_top components

--- src/components/General-info.jsx
+++ src/components/General-info.jsx
@@ -1,2 +1,16 @@
 import { useState } from "react";
 
+function General_top() {
+    return (
+        <h1>General Information</h1>
+    );
+
+}
+
+function GeneralInfo() {
+    return (
+        <General_top></General_top>
+    );
+}
+
+export { GeneralInfo };
