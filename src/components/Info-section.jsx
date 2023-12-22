87133bf1fcf8d8186ceb883cf9a9ea37461ef385
CameronTheDeveloper <Cameronposey@outlook.com>
2023-12-22 04:04:30 -0800

    Add InfoSection component to display input & info

---
+++ src/components/Info-section.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function InfoSection({ inputs, info }) {
+    const [inputMode, setInputMode] = useState(true);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setInputMode(false);
+    }
+
+    function handleEdit() {
+        setInputMode(true);
+    }
+
+    return (
+        <div className='info-section'>
+            {inputMode ? inputs : info}
+            {inputMode ?
+                <button className='submit-button' onClick={handleSubmit}>Submit</button>
+                : <button className='edit-button' onClick={handleEdit}>Edit</button>}
+        </div>
+    );
+}
+
+export default InfoSection;
