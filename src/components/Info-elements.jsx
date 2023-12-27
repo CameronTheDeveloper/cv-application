a5be03205181509b8a91ee73c8fca54810bb1b3d
CameronTheDeveloper <Cameronposey@outlook.com>
2023-12-26 23:15:06 -0800

    Remove unneeded React fragment in Info

--- src/components/Info-elements.jsx
+++ src/components/Info-elements.jsx
@@ -1,11 +1,9 @@
 function Info({ data, handleEdit, infoClass }) {
     return (
-        <>
-            <div className='info'>
-                <div className={infoClass}><h1>{data}</h1></div>
-                <button onClick={handleEdit}>Edit</button>
-            </div>
-        </>
+        <div className='info'>
+            <div className={infoClass}><h1>{data}</h1></div>
+            <button onClick={handleEdit}>Edit</button>
+        </div>
     );
 }
 
