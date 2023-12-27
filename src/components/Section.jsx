bb52a4722ea8a332eedccef0fe0046bcdd23c5d8
CameronTheDeveloper <Cameronposey@outlook.com>
2023-12-27 03:15:51 -0800

    Add Section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,8 @@
+export default function Section(sectionTitle, sectionID, children) {
+    return (
+        <div className='section' id={sectionID}>
+            <h2 className='section-title'>{sectionTitle}</h2>
+            <div>{children}</div>
+        </div>
+    );
+}
